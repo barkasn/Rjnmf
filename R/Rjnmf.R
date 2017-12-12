0bd07b462fad9bfcdb6c582cdb6a084ce426e840
barkasn <Nikolas.barkas@outlook.com>
2017-12-12 11:14:08 -0500

    fix seed setting

--- R/Rjnmf.R
+++ R/Rjnmf.R
@@ -7,14 +7,18 @@ NULL
 #' Performd Joint NMF for two matrices
 #' @export Rjnmf
 Rjnmf <- function(Xs, Xu, k, alpha, lambda, epsilon, maxiter, verbose) {
+    set.seed(4123);
+
    ret <- RjnmfC(Xs, Xu, k, alpha, lambda, epsilon, maxiter, verbose);
    names(ret) <- c('Hs','Hu','W');
    ret;
 }
 
 Rjnmf <- function(Xs, Xu, A, k, alpha, beta, lambda, epsilon, matiter, verbose) {
+    set.seed(4123);
+
     ret <- RjnmfGRC(Xs, Xu, A, k, alpha, beta, lambda, epsilon, maxiter, verbose);
     names(ret) <- c('Hs','Hu','W');
     ret;
-    
+
 }
