51f14ef76d33febe6bff4a7c7f5d2a424fe56f0e
Nikolas Barkas <Nikolaos_Barkas@hms.harvard.edu>
2017-12-14 13:19:43 -0500

    add seed param

--- R/Rjnmf.R
+++ R/Rjnmf.R
@@ -6,8 +6,8 @@ NULL
 
 #' Performd Joint NMF for two matrices
 #' @export Rjnmf
-Rjnmf <- function(Xs, Xu, k, alpha, lambda, epsilon, maxiter, verbose) {
-    set.seed(4123);
+Rjnmf <- function(Xs, Xu, k, alpha, lambda, epsilon, maxiter, verbose, seed =  4123) {
+    set.seed(seed);
 
    ret <- RjnmfC(Xs, Xu, k, alpha, lambda, epsilon, maxiter, verbose);
    names(ret) <- c('Hs','Hu','W');
@@ -16,8 +16,8 @@ Rjnmf <- function(Xs, Xu, k, alpha, lambda, epsilon, maxiter, verbose) {
 
 #' Perform Joint NMF with graph regularisation
 #' @export RjnmfGR
-RjnmfGR <- function(Xs, Xu, A, k, alpha, beta, lambda, epsilon, maxiter, verbose) {
-    set.seed(4123);
+RjnmfGR <- function(Xs, Xu, A, k, alpha, beta, lambda, epsilon, maxiter, verbose, seed= 4123) {
+    set.seed(seed);
 
     ret <- RjnmfGRC(Xs, Xu, A, k, alpha, beta, lambda, epsilon, maxiter, verbose);
     names(ret) <- c('Hs','Hu','W');
